5c5d3efc5bf0888cafa583ba2367c4186afc2e09
William van Schooneveld <w.schooneveld@gmail.com>
2023-05-10 10:09:12 +0200

    Yeager working in own project

---
+++ src/main/java/han/Main.java
@@ -0,0 +1,19 @@
+package han;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+public class Main extends YaegerGame {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void setupScenes() {
+
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module waterworld {
+    requires hanyaeger;
+
+    exports han;
+
+//    opens audio;
+//    opens backgrounds;
+//    opens sprites;
+}
